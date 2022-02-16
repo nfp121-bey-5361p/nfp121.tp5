da585426864f2a788ca32d0a948a5f6fc8ad48b1
Johnny Makhoul <johnny.makhoul@isae.edu.lb>
2022-02-16 17:36:23 +0200

    Add generic type

--- tp5/question3/HashSetFactory.java
+++ tp5/question3/HashSetFactory.java
@@ -3,7 +3,7 @@ package question3;
 import java.util.Set;
 import java.util.HashSet;
 
-public class HashSetFactory<T extends Comparable> implements Factory<Set<T>> {
+public class HashSetFactory<T extends Comparable<T>> implements Factory<Set<T>> {
     
     @Override
     public Set<T> create() {
--- tp5/question3/TreeSetFactory.java
+++ tp5/question3/TreeSetFactory.java
@@ -3,7 +3,7 @@ package question3;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class TreeSetFactory<T extends Comparable> implements Factory<Set<T>> {
+public class TreeSetFactory<T extends Comparable<T>> implements Factory<Set<T>> {
 
     @Override
     public Set<T> create() {
